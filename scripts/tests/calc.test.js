b4b4ede6bb94fd63462794db80d3985136077686
JKirkby95 <>
2023-09-27 18:17:14 +0000

    jest testing complete

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,21 @@
+const addition = require("./calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("should return 20 for 10 + 10", () => {
+            expect(addition(10, 10)).toBe(20);
+        });
+    });
+    describe("Subtract function", () => {
+
+    });
+    describe("Multiply function", () => {
+
+    });
+    describe("Divide function", () => {
+
+    });
+});
